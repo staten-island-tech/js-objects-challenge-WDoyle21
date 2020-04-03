f66a8cb259fef92889dcf4d87fcab1aab266f2eb
WDoyle21 <57500204+WDoyle21@users.noreply.github.com>
2020-04-03 09:54:50 -0400

    432

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const Riya = {
+/* const Riya = {
     name: "Riya",
     dob: 2003,
     graduated: false,
@@ -6,4 +6,39 @@ const Riya = {
         console.log(this);
         return 2020 - Riya.dob;
     }
-}
+}; */
+
+/* const Homer = {
+    name:"Homer",
+    age: 32,
+    family:{
+        wife:{
+            name: "Marge",
+            age: 32
+        },
+        son: {
+            name: "Bart",
+            age: 10
+        }
+    },
+    pets: ["cat","dog"]
+};
+
+Homer.family.daughter = {
+    name: "Lisa",
+    age: 8
+}; */
+
+/* const {name, age} = Riya;
+
+const calc = {
+    add: function() {
+        return x + y;
+    }
+};
+const { add, divided, cosine } = calc;
+
+const { name, poster} = movie; */
+
+/*const Angel = {};
+Angel.dob = Riya.dob;*/
